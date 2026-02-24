a81e94fa0542ee741907875b051202463a350e5b
iastechnologies321 <iastechnologies321@gmail.com>
2026-02-24 10:52:28 +0530

    added few features

--- tests/myTest.spec.ts
+++ tests/myTest.spec.ts
@@ -87,23 +87,92 @@ test('Test Case 1',async({page}) => {
         // await page.screenshot({path:'./screenshots/dragAndDrop03.png'});
 
     // alerts - 
-        await page.goto('https://demo.automationtesting.in/Alerts.html');
-        await page.locator('a[href="#OKTab"]').click();    
-        let message = "";
-        page.on('dialog',(alert)=>{
-            message = alert.message();
-            console.log(message);
-            alert.accept()
-        })
-        await page.locator('.btn.btn-danger').click(); 
-        await expect(message).toEqual('I am an alert box!') 
-        await page.screenshot({path:'./screenshots/alert01.png'});
+        // await page.goto('https://demo.automationtesting.in/Alerts.html');
+        // await page.locator('a[href="#OKTab"]').click();    
+        // let message = "";
+        // page.on('dialog',(alert)=>{
+        //     message = alert.message();
+        //     console.log(message);
+        //     alert.accept()
+        // })
+        // await page.locator('.btn.btn-danger').click(); 
+        // await expect(message).toEqual('I am an alert box!') 
+        // await page.screenshot({path:'./screenshots/alert01.png'});
+
+
+        // alert with text box
+        // await page.goto('https://demo.automationtesting.in/Alerts.html');
+        // await page.locator('a[href="#Textbox"]').click();    
+        // let name = 'Nirvan';
+        // page.on('dialog',(alert)=>{
+        //     console.log(alert.message());
+        //     alert.accept(name)
+        // });
+        // await page.locator('#Textbox button').click();
+
+        // await page.screenshot({path:'./screenshots/alert03.png'});
+        // await expect(page.locator('#demo1')).toHaveText(`Hello ${name} How are you today`);
+
+
+        // downlaods
+        // Way - 1
+        // await page.goto('https://demo.automationtesting.in/FileDownload.html');
+        // let downloadPromise = page.waitForEvent('download');
+        // await page.locator('.btn.btn-primary').click();
+        // let download = await downloadPromise;
+        // await download.saveAs(`./downloads/${download.suggestedFilename()}`)
+        // await page.screenshot({path:'./screenshots/download01.png'});
+
+        // Way 2 - 
+    //     await page.goto('https://demo.automationtesting.in/FileDownload.html');
+    //     const [download] = await Promise.all([
+    //         page.waitForEvent('download'),
+    //         page.locator('.btn.btn-primary').click()
+    //     ]
+    // )
+    //     await download.saveAs(`./downloads/${download.suggestedFilename()}`)
+    //     await page.screenshot({path:'./screenshots/download02.png'});
+
+
+    // state storage
+        // await page.goto('https://automationexercise.com/');
+        // await expect(page).toHaveURL('https://automationexercise.com/');
+        // await page.waitForTimeout(10000);
+        
+        // await page.locator('[href="/login"]').click();
+        // await page.locator('[data-qa="login-email"]').fill('iastechnologies321@gmail.com');
+        // await page.locator('[data-qa="login-password"]').fill('iastechnologies321');
+        // await page.locator('[data-qa="login-button"]').click();
+        // await page.waitForURL('https://automationexercise.com/');
+        // await page.context().storageState({path:"./auth/login.json"})
+
+
+    // frames
+        await page.goto('https://demo.automationtesting.in/Frames.html');
+        await page.frameLocator('#singleframe').locator('input[type="text"]').first().fill('I am inside Frame');
+        await page.screenshot({path:'./screenshots/frames01.png'});
+
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
     // assignment - 
     // https://demo.automationtesting.in/Dynamic.html
+    // https://demo.automationtesting.in/Frames.html
+    // https://demo.automationtesting.in/Frames.html
 
 
 
